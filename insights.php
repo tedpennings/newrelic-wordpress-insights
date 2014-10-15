0936bb012326e36e8a0b805765263cbf93ba11e3
Ted Pennings <tedpennings@gmail.com>
2014-10-14 22:32:23 -0700

    style cleanup

--- insights.php
+++ insights.php
@@ -13,20 +13,20 @@ Author URI: http://newrelic.com
 */
 
 function add_newrelic_insights_attributes() {
-  if (!function_exists("newrelic_add_custom_parameter")) {
-    return;
+  if (!function_exists('newrelic_add_custom_parameter')) {
+    return FALSE;
   }
   if ( is_user_logged_in() ) {
     $current_user = wp_get_current_user();
-    newrelic_add_custom_parameter("wp_logged_in", true);
-    newrelic_add_custom_parameter("wp_username", $current_user->user_login);
-    newrelic_add_custom_parameter("wp_user_id", $current_user->ID);
-    newrelic_add_custom_parameter("wp_user_email", $current_user->user_email);
+    newrelic_add_custom_parameter('wp_logged_in', true);
+    newrelic_add_custom_parameter('wp_username', $current_user->user_login);
+    newrelic_add_custom_parameter('wp_user_id', $current_user->ID);
+    newrelic_add_custom_parameter('wp_user_email', $current_user->user_email);
   } else {
-    newrelic_add_custom_parameter("wp_logged_in", false);
+    newrelic_add_custom_parameter('wp_logged_in', false);
   }
 }
 
-add_action( 'init', 'add_newrelic_insights_attributes' );
+add_action('init', 'add_newrelic_insights_attributes');
 
 ?>
